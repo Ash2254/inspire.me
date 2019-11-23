d4ab882ed0de95ecc155c9f81e3c5b2fc8540c53
Ash2254 <47071224+Ash2254@users.noreply.github.com>
2019-11-23 08:14:36 -0800

    Began login page

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
 
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css'/>
 
     <!-- Material Dashboard CSS -->
     <link rel="stylesheet" href="assets/css/style.css">
